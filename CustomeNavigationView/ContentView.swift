8e0bd0804425fabebf6b1d80171c0a3cf71499b7
MadeReihan <reihanmade@gmail.com>
2023-12-09 16:17:05 +0700

    navigation bar item

--- CustomeNavigationView/ContentView.swift
+++ CustomeNavigationView/ContentView.swift
@@ -9,10 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("hello world123")
+        Home()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct Home:View {
+    var body: some View {
+        NavigationView(){
+            Text("Hello World")
+            
+                .navigationBarItems(leading: HStack{
+                    
+                    
+                    Button(action:{
+                        print("hello")
+                    }){
+                        Image("LogoDicoding").renderingMode(.original).resizable().frame(width: 90, height: 20)
+                    }
+                    
+                    
+                    
+                }, trailing: HStack(spacing:10){
+                    Button(action:{
+                        print("hello print")
+                    }){
+                        Image(systemName: "tray.full").foregroundColor(.secondary)
+                    }
+                    Button(action:{
+                        print("hello print")
+                    }){
+                        Image(systemName: "video.fill").foregroundColor(.secondary)
+                    }
+                    Button(action:{
+                        print("hello print")
+                    }){
+                        Image(systemName: "magnifyingglass").foregroundColor(.secondary)
+                    }
+
+                    Button(action:{
+                        print("hello print")
+                    }){
+                        Image("MyPicture").renderingMode(.original).resizable().frame(width: 20, height: 20).clipShape(Circle())
+                    }
+                }
+                                    
+                )
+        }
+    }
 }
