02d5c7abcde3259aff5d8c0fd13074260f4479eb
made reihan <reihanmade@gmail.com>
2023-12-09 10:20:00 +0700

    Initial Commit

---
+++ DasarSwiftUi1/DasarSwiftUi1App.swift
@@ -0,0 +1,17 @@
+//
+//  DasarSwiftUi1App.swift
+//  DasarSwiftUi1
+//
+//  Created by made reihan on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DasarSwiftUi1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
