56da7c3f6ae53cf583ef2505703c3a1c70f04d3f
made reihan <reihanmade@gmail.com>
2023-12-09 14:04:47 +0700

    Initial Commit

---
+++ CustomeNavigationView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomeNavigationView
+//
+//  Created by made reihan on 09/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomeNavigationView/CustomeNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomeNavigationViewApp.swift
+//  CustomeNavigationView
+//
+//  Created by made reihan on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomeNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
