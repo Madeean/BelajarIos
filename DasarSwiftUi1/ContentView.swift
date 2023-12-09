7ed8e8631ad662c8a32c83e0030b1945a782967a
MadeReihan <reihanmade@gmail.com>
2023-12-09 15:02:34 +0700

    selesai

--- DasarSwiftUi1/ContentView.swift
+++ DasarSwiftUi1/ContentView.swift
@@ -9,16 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("uname").resizable().edgesIgnoringSafeArea(.all)
+            VStack(spacing:20){
+                handImageView()
+                FormBox()
+            }.padding(.all,20)
         }
-        .padding()
+        
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct handImageView:View{
+    var body: some View{
+        VStack{
+            Image("LogoChallenge")
+                .resizable()
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color("warnaku"))
+                .cornerRadius(20)
+            
+            Text("Hello Swift UI").foregroundColor(.white)
+        }
+    }
+}
+
+struct FormBox:View {
+    
+    @State var username:String = ""
+    @State var password:String = ""
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("Username").font(.callout).bold()
+            TextField("Username...", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Password").font(.callout).bold()
+            SecureField("Password...", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            
+            Button(action: {
+                print("hello")
+            }, label: {
+                HStack{
+                    Spacer()
+                    Text("Sign in")
+                    Spacer()
+                }
+            }).padding().background(.black).cornerRadius(10).foregroundColor(.white)
+        }.padding(.all,30).background(.orange).cornerRadius(10)
+    }
 }
