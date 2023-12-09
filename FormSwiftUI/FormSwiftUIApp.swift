7b2d6018cf6cb9801ea2d02c0a22dedd6ea14c05
made reihan <reihanmade@gmail.com>
2023-12-09 12:24:26 +0700

    Initial Commit

---
+++ FormSwiftUI/FormSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormSwiftUIApp.swift
+//  FormSwiftUI
+//
+//  Created by made reihan on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FormSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
