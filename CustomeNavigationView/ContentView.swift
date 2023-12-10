3bdcdf4dafccf72c6015685b139e0b3f2f2a1832
MadeReihan <reihanmade@gmail.com>
2023-12-10 15:33:37 +0700

    tab bar

--- CustomeNavigationView/ContentView.swift
+++ CustomeNavigationView/ContentView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Home()
+//        Home()
+        ZStack{
+            TabView{
+                Home()
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Beranda")
+                        
+                    }
+                Home()
+                    .tabItem {
+                        Image(systemName: "paperplane.fill")
+                        Text("Explorasi")
+                        
+                    }
+                Home()
+                    .tabItem {
+                        Image(systemName: "tray.fill")
+                        Text("Subscription")
+                        
+                    }
+                Home()
+                    .tabItem {
+                        Image(systemName: "envelope.fill")
+                        Text("Kotak Masuk")
+                        
+                    }
+                Home()
+                    .tabItem {
+                        Image(systemName: "play.rectangle.fill")
+                        Text("Koleksi")
+                        
+                    }
+            }.accentColor(.red)
+        }
     }
 }
 
@@ -21,124 +55,125 @@ struct Home: View {
     var body: some View {
         NavigationView {
             Konten()
+                .navigationBarTitle("")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(leading: HStack {
+                    Button(action: {
+                        print(
+                            "hello"
+                        )
+                    }) {
+                        Image(
+                            "LogoDicoding"
+                        ).renderingMode(
+                            .original
+                        ).resizable().frame(
+                            width: 90,
+                            height: 20
+                        )
+                    }
 
-            .navigationBarItems(leading: HStack {
-                Button(action: {
-                    print(
-                        "hello"
-                    )
-                }) {
-                    Image(
-                        "LogoDicoding"
-                    ).renderingMode(
-                        .original
-                    ).resizable().frame(
-                        width: 90,
-                        height: 20
-                    )
-                }
+                },
+                trailing: HStack(
+                    spacing: 10
+                ) {
+                    Button(action: {
+                        print(
+                            "hello print"
+                        )
+                    }) {
+                        Image(
+                            systemName: "tray.full"
+                        ).foregroundColor(
+                            .secondary
+                        )
+                    }
+                    Button(action: {
+                        print(
+                            "hello print"
+                        )
+                    }) {
+                        Image(
+                            systemName: "video.fill"
+                        ).foregroundColor(
+                            .secondary
+                        )
+                    }
+                    Button(action: {
+                        print(
+                            "hello print"
+                        )
+                    }) {
+                        Image(
+                            systemName: "magnifyingglass"
+                        ).foregroundColor(
+                            .secondary
+                        )
+                    }
 
-            },
-            trailing: HStack(
-                spacing: 10
-            ) {
-                Button(action: {
-                    print(
-                        "hello print"
-                    )
-                }) {
-                    Image(
-                        systemName: "tray.full"
-                    ).foregroundColor(
-                        .secondary
-                    )
+                    Button(action: {
+                        print(
+                            "hello print"
+                        )
+                    }) {
+                        Image(
+                            "MyPicture"
+                        ).renderingMode(
+                            .original
+                        ).resizable().frame(
+                            width: 20,
+                            height: 20
+                        ).clipShape(
+                            Circle()
+                        )
+                    }
                 }
-                Button(action: {
-                    print(
-                        "hello print"
-                    )
-                }) {
-                    Image(
-                        systemName: "video.fill"
-                    ).foregroundColor(
-                        .secondary
-                    )
-                }
-                Button(action: {
-                    print(
-                        "hello print"
-                    )
-                }) {
-                    Image(
-                        systemName: "magnifyingglass"
-                    ).foregroundColor(
-                        .secondary
-                    )
-                }
-
-                Button(action: {
-                    print(
-                        "hello print"
-                    )
-                }) {
-                    Image(
-                        "MyPicture"
-                    ).renderingMode(
-                        .original
-                    ).resizable().frame(
-                        width: 20,
-                        height: 20
-                    ).clipShape(
-                        Circle()
-                    )
-                }
-            }
             )
+            
         }
     }
 }
 
-
-struct Konten:View {
-    let listModel:[Model] = Model.getModel()
+struct Konten: View {
+    let listModel: [Model] = Model.getModel()
     var body: some View {
-        List(listModel){ item in
-            KontenItem(model: item).background(.green)
+        List() { 
+            ForEach(listModel) { model in
+                KontenItem(model: model)
+            }
         }
     }
 }
 
-struct KontenItem:View {
-    let model:Model
+struct KontenItem: View {
+    let model: Model
     var body: some View {
-        VStack{
-            ZStack(alignment:.bottomTrailing){
-                Image("uname").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/).cornerRadius(8)
-                
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image("uname").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/ .fill/*@END_MENU_TOKEN@*/).cornerRadius(8)
+
                 Text("10:00")
-                    .padding(.all,5)
+                    .padding(.all, 5)
                     .foregroundColor(.white)
                     .font(.caption)
                     .background(.black)
                     .cornerRadius(5)
-                    .padding(.trailing,5)
-                    .padding(.bottom,5)
+                    .padding(.trailing, 5)
+                    .padding(.bottom, 5)
             }
             Spacer(minLength: 10)
-            
-            HStack{
-                Image("MyPicture").resizable().frame(width: 30, height: 30).clipShape(Circle()).padding(.trailing,10)
-                
-                VStack(alignment: .leading){
+
+            HStack {
+                Image("MyPicture").resizable().frame(width: 30, height: 30).clipShape(Circle()).padding(.trailing, 10)
+
+                VStack(alignment: .leading) {
                     Text(model.judul).font(.headline)
                     Text(model.description).lineLimit(2)
                 }
-                
+
                 Spacer()
-                Image(systemName: "list.bullet").padding(.leading,10)
+                Image(systemName: "list.bullet").padding(.leading, 10)
             }
-        }.padding(.all,8).cornerRadius(16)
-        
-        Spacer()
+        }.padding(.all, 8).cornerRadius(16)
     }
 }
