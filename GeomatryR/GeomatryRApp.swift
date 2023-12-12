863263ca365cae713f43af82e921dde870d1490e
MadeReihan <reihanmade@gmail.com>
2023-12-12 21:37:13 +0700

    Initial Commit

---
+++ GeomatryR/GeomatryRApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeomatryRApp.swift
+//  GeomatryR
+//
+//  Created by made reihan on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GeomatryRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
