3f30805b6fd64705d81aaecbdc21d8b4b6f22870
MadeReihan <reihanmade@gmail.com>
2023-12-13 20:59:38 +0700

    Initial Commit

---
+++ TapedApp/TapedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapedAppApp.swift
+//  TapedApp
+//
+//  Created by made reihan on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TapedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
