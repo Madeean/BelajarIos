c39be16942588f4b5778fc82cb77383c3f4b5854
MadeReihan <reihanmade@gmail.com>
2023-12-14 10:53:19 +0700

    Initial Commit

---
+++ NestedModel/NestedModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestedModelApp.swift
+//  NestedModel
+//
+//  Created by made reihan on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NestedModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
