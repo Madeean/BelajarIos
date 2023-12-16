2d0dfb3884c79e6a6d8e320dea77a9e19f4a593c
MadeReihan <reihanmade@gmail.com>
2023-12-16 20:09:10 +0700

    Initial Commit

---
+++ LoadingImageSwiftUI/LoadingImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingImageSwiftUIApp.swift
+//  LoadingImageSwiftUI
+//
+//  Created by made reihan on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LoadingImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
