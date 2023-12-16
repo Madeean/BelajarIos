57b0b1506c4fc9363883338d6e72de5bcf14dcd0
MadeReihan <reihanmade@gmail.com>
2023-12-16 23:05:06 +0700

    like bottom sheet

--- DragGesture/ContentView.swift
+++ DragGesture/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showMenu: Bool = false
+    @State var offset = CGSize.zero
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        let drag = DragGesture()
+            .onChanged{ gesture in
+                self.offset = gesture.translation
+                
+            }.onEnded{value in
+                self.offset = CGSize.zero
+                self.showMenu = false
+            }
+        
+        return ZStack{
+            Color.orange.edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("LogoChallenge").resizable().frame(width: 300, height: 300)
+                
+                Button(action: {
+                    withAnimation{
+                        self.showMenu.toggle()
+                    }
+                }, label: {
+                    Text("Promo Merdeka").padding().background(.green).foregroundColor(.white).cornerRadius(20)
+                })
+            }
+            
+            
+            if showMenu{
+                Kupon().transition(.move(edge: .bottom).animation(.default)).offset(y:self.offset.height).gesture(drag)
+            }
         }
-        .padding()
+        
+    }
+}
+
+struct Kupon:View {
+    var body: some View {
+        VStack{
+            Text("anda dapet kupon merdeka").font(.largeTitle).bold().foregroundColor(.white)
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity).background(.red).cornerRadius(20).edgesIgnoringSafeArea(.bottom)
     }
 }
 
